079d99172df1ecbfaecd35a5b0ad0a4d32a93d5e
andriusandriusandrius <and.zurba@gmail.com>
2025-11-11 17:27:05 +0200

    fix database connection factory dependency injection

--- backend/Program.cs
+++ backend/Program.cs
@@ -2,7 +2,11 @@ using backend.Database;
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
-builder.Services.AddSingleton<DatabaseConnectionFactory>();
+builder.Services.AddSingleton(sp =>
+{
+    var configuration = sp.GetRequiredService<IConfiguration>();
+    return new DatabaseConnectionFactory(configuration);
+});
 builder.Services.AddSingleton<MigrationRunner>();
 
 using (var scope = app.Services.CreateScope())
@@ -10,5 +14,6 @@ using (var scope = app.Services.CreateScope())
     var migrationRunner = scope.ServiceProvider.GetRequiredService<MigrationRunner>();
     await migrationRunner.ApplyMigrationsAsync();
 }
+
 
 app.Run();
