7e6368850bd7849c98e415e2e42fd13ebcee1d75
andriusandriusandrius <and.zurba@gmail.com>
2025-11-11 17:19:55 +0200

    add program.cs

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,6 +1,14 @@
+using backend.Database;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
+builder.Services.AddSingleton<DatabaseConnectionFactory>();
+builder.Services.AddSingleton<MigrationRunner>();
 
-app.MapGet("/", () => "Hello World!");
+using (var scope = app.Services.CreateScope())
+{
+    var migrationRunner = scope.ServiceProvider.GetRequiredService<MigrationRunner>();
+    await migrationRunner.ApplyMigrationsAsync();
+}
 
 app.Run();
