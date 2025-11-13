14e03ffb2c96f4459c2db4e44236a44caeabebb7
andriusandriusandrius <and.zurba@gmail.com>
2025-11-13 20:27:29 +0200

    user models

---
+++ backend/Models/User/Admin.cs
@@ -0,0 +1,7 @@
+namespace backend.Models.User
+{
+    public class Admin : User
+    {
+         //no implementation yet
+    }
+}
---
+++ backend/Models/User/Barista.cs
@@ -0,0 +1,7 @@
+namespace backend.Models.User
+{
+    public class Barista : User
+    {
+        //no implementation yet
+    }
+}
